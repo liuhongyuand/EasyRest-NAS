22c8d620fa3c8ef18884df9f113855dd4793064a
Lhy <liuhongyu.louie@gmail.com>
2018-06-08 23:11:27 +0800

    Fixed bug that will be made compile error.

--- EasyRest/src/main/java/tech/dbgsoftware/easyrest/ioc/remote/EasyRestServiceLookup.java
+++ EasyRest/src/main/java/tech/dbgsoftware/easyrest/ioc/remote/EasyRestServiceLookup.java
@@ -31,7 +31,7 @@ public class EasyRestServiceLookup {
         if (EasyRestDistributedServiceBind.getLocalService().contains(service.getName())){
             return BeanOperationUtils.getBeansFromInterface(service, beanName);
         } else {
-            return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class[]{service}, EasyRestProxyFactory.getProxyInstanceWithTimeout(beanName, connectionMillis, resultMillis));
+            return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class[]{service}, EasyRestProxyFactory.getProxyInstanceWithBeanNameAndTimeout(beanName, connectionMillis, resultMillis));
         }
     }
 
