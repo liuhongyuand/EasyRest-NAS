02495e41a0716bf05f2ad9a01d975b6b545b4841
Lhy <liuhongyu.louie@gmail.com>
2019-03-25 00:20:03 +0800

    Add example to display how to set cross site host IP for EasyRest.

--- Example/Example-Quick-Start/src/main/java/com/example/Example.java
+++ Example/Example-Quick-Start/src/main/java/com/example/Example.java
@@ -2,12 +2,13 @@ package com.example;
 
 
 import tech.dbgsoftware.easyrest.EasyRest;
+import tech.dbgsoftware.easyrest.network.NettyInit;
 
 public class Example {
 
     public static void main(String[] args) {
         EasyRest easyRest = new EasyRest("classpath:MyExampleApplicationContext.xml");
-        easyRest.startup("EasyRestServer", 8080);
+        easyRest.startup("EasyRestServer", new NettyInit("127.0.0.1", 8080));
     }
 
 }
